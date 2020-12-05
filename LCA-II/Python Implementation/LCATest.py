087be3187d9f69fcb2232076cc7de0efe7782690
Aoife <simma@tcd.ie>
2020-12-05 03:29:21 +0000

    Added test outlines for DAG functionality

--- LCA-II/Python Implementation/LCATest.py
+++ LCA-II/Python Implementation/LCATest.py
@@ -4,34 +4,61 @@ from Node import Node
 
 class testLCA(unittest.TestCase):
 
-    def testContains(self):
+    # Test to check tree building functionality
+    def testNode(self):
 
-        # Create tree
+        #self.assertEqual(Node(0).children, [], "Should not contain any nodes")
+
+        #Create tree
+        # 1 = [2, 3, 4, 5], 2 = [4], 3 = [4, 5], 4 = [5], 5 = []
         root = Node(1)
-        root.left = Node(2)
-        root.left.left = Node(3)
-        root.left.right = Node(4)
-        root.right = Node(5)
-        root.right.left = Node(6)
-        root.right.right = Node(7)
+        # addChildNode(root, parentNodeID, childNodeID)
+        # addChildNode(root, 1, 2)
+        # addChildNode(root, 2, 4)
 
-        self.assertTrue(LCA.contains(root, Node(3)), "Check if tree with root provided contains a specified node")
-        self.assertFalse(LCA.contains(root, Node(9)), "Check contains() with node not in tree")
+        #check children
+        listID = []
+        #for i in range(len(root.children)):
+        #    listID.append(root.children[i])
+        #self.assertEqual(listID, [2, 3, 4, 5], "Check if all children can be found from the root node")
+        
+        #currNode = searchNode(root, nodeID)
+        listID.clear()
+        #for i in range(len(currNode.children)):
+        #    listID.append(currNode.id)
+        #self.assertEqual(listID, [4], "Check if non-root node can be found and accessed")
+
+        # Try to add a node cyclically
+        # self.assertEqual(addChildNode(root, 5, 1), -1, "Checks node can't be added cyclically")
+
+    def testSearchNode(self):
+
+        # Check None arguments
+
+        #Create tree
+        # 1 = [2, 3, 4, 5], 2 = [4], 3 = [4, 5], 4 = [5], 5 = []
+        root = Node(1)
+        # addChildNode(root, parentNodeID, childNodeID)
+        # addChildNode(root, 1, 2)
+        # addChildNode(root, 2, 4)
+
+        #self.assertTrue(Node.searchNode(root, Node(3)), "Check if tree with root provided contains a specified node")
+        #self.assertFalse(Node.searchNode(root, Node(9)), "Check contains() with node not in tree")
 
     def testgetLCA(self):
 
-        # Create tree
-        root = Node(1)
-        root.left = Node(2)
-        root.left.left = Node(3)
-        root.left.right = Node(4)
-        root.right = Node(5)
-        root.right.left = Node(6)
-        root.right.right = Node(7)
+        # Check None arguments
 
-        self.assertEqual(LCA.getLCA(root, Node(3), Node(4)).id, 2, "Check LCA of two nodes contained in tree")
-        self.assertEqual(LCA.getLCA(root, Node(7), Node(2)).id, 1, "Check getLCA() with nodes of different depths")
-        self.assertIsNone(LCA.getLCA(root, Node(0), Node(4)), "Check getLCA() when one node is not on tree")
+        #Create tree
+        # 1 = [2, 3, 4, 5], 2 = [4], 3 = [4, 5], 4 = [5], 5 = []
+        root = Node(1)
+        # addChildNode(root, parentNodeID, childNodeID)
+        # addChildNode(root, 1, 2)
+        # addChildNode(root, 2, 4)
+
+        #self.assertEqual(LCA.getLCA(root, [4, 5]).id, 2, "Check LCA of two nodes contained in tree")
+        #self.assertEqual(LCA.getLCA(root, [4, 5, 2]).id, 2, "Check LCA of three nodes contained in tree")
+        #self.assertIsNone(LCA.getLCA(root, [2, 8]), "Check getLCA() when one node is not on tree")
         
 
 
---
+++ LCA-II/README.md
@@ -0,0 +1,1 @@
+DAG will be added to Python implementation
