8d9d4846813997930adc28572d4bbb9730727075
Aoife <simma@tcd.ie>
2020-12-17 02:59:35 +0000

    Cleaned code, added README"

---
+++ Accessing-REST/README.md
@@ -0,0 +1,7 @@
+# Accessing REST API
+
+Code for my initial exploration of the Github API
+
+## Requirements
+
+Uses Requests library for Python. Documentation available [here](https://buildmedia.readthedocs.org/media/pdf/requests/master/requests.pdf)
--- Accessing-REST/setup.py
+++ Accessing-REST/setup.py
@@ -1,11 +1,31 @@
 # python -m pip install requests
 import requests
-user = "simmma"
-resp = requests.get('https://api.github.com/users/' + user)
-print(resp)
-if resp.status_code != 200:
-    # This means something went wrong.
-    # raise ApiError('GET /tasks/ {}'.format(resp.status_code))
-    print("oops")
-else:
-    print(resp.json())
+
+url = "https://api.github.com/"
+
+def checkStatus(resp):
+    if resp.status_code != 200:
+        print("oops")
+        return -1
+    return 0
+
+def basicAPICall():
+    # First call to github API
+    resp = requests.get(url)
+    if(checkStatus(resp)):
+        return ""
+    return resp.text
+
+def getUser(user):
+    # Get list of repos of a passed user
+    userReq = "users/" + user
+    resp = requests.get(url + userReq)
+    if(checkStatus(resp)):
+        return ""
+    return resp.text
+    
+def main():
+    # print(basicAPICall())
+    print(getUser("simmma"))
+
+main()
