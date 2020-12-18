845b316c4ceb3e58bf028ae7554aa690335ad14d
Aoife <simma@tcd.ie>
2020-12-18 13:39:58 +0000

    Added some more basic API calls

--- Accessing-REST/README.md
+++ Accessing-REST/README.md
@@ -5,3 +5,9 @@ Code for my initial exploration of the Github API
 ## Requirements
 
 Uses Requests library for Python. Documentation available [here](https://buildmedia.readthedocs.org/media/pdf/requests/master/requests.pdf)
+
+2.1.1 To install Requests, simply run this simple command in your terminal of choice:
+`$ python -m pip install requests`
+
+
+Some calls require a Github Personal access token. This an be found a Settings->Developer settings->Personal access tokens
--- Accessing-REST/setup.py
+++ Accessing-REST/setup.py
@@ -1,31 +1,59 @@
 # python -m pip install requests
 import requests
+import json
 
 url = "https://api.github.com/"
+user = "simmma"
+token = ""  # Enter valid token here!
 
 def checkStatus(resp):
     if resp.status_code != 200:
         print("oops")
+        print(resp.json()["message"])
         return -1
     return 0
 
-def basicAPICall():
+def basicAPICall(args):
     # First call to github API
-    resp = requests.get(url)
+    resp = requests.get(url + args)
     if(checkStatus(resp)):
         return ""
-    return resp.text
+    return resp
 
-def getUser(user):
-    # Get list of repos of a passed user
-    userReq = "users/" + user
+def getUserPublicRepos():
+    # Get list of public repo names of a passed user
+    userReq = "users/" + user + "/repos"
     resp = requests.get(url + userReq)
     if(checkStatus(resp)):
         return ""
-    return resp.text
-    
+    #print(type(resp.content)) # class 'bytes'
+    #print(type(resp.json())) # class 'list'
+    #print(type(resp.json()[0])) # class 'dict'
+    resp = resp.json()
+    names = [];
+    for i in range(len(resp)):
+        names.append(resp[i]['name'])
+    return names
+
+def getUserAllRepos():
+    if token == "":
+        print("Please enter a valid token!")
+        return 
+    resp = requests.get(url + "search/repositories?q=user:" + user, auth=(user, token))
+    if(checkStatus(resp)):
+        return ""
+    #resp = resp.json()
+    items = resp.json()["items"]
+    names = [];
+    for i in range(len(items)):
+        names.append([items[i]['name'], ("Private" if (items[i]['private']) else "Public")])
+    return names
+    #return items
+
+
 def main():
-    # print(basicAPICall())
-    print(getUser("simmma"))
+    #print(basicAPICall(""))
+    print("Public repo names for", user, "\n", getUserPublicRepos(), "\n")
+    print("All repo names and privacy status for", user, "\n",getUserAllRepos())
 
 main()
